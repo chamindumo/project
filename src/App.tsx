f0ff34dc7b57548327a6ce0fd53d36657ddb8f44
chamindumo <chamindu.20210091@iit.ac.lk>
2025-03-22 14:10:46 +0530

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Layout } from './components/Layout';
 import { HomePage } from './pages/HomePage';
 import { AnalyzePage } from './pages/AnalyzePage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { AboutUsPage } from './pages/AboutUsPage';
+import { HelpPage } from './pages/HelpPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="analyze" element={<AnalyzePage />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="help" element={<HelpPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
