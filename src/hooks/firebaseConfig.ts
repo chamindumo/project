31713b62034969d5ce414c1671eb31383bc813dc
chamindumo <chamindu.20210091@iit.ac.lk>
2025-03-28 21:46:29 +0530

    add fore base

--- src/hooks/firebaseConfig.ts
+++ src/hooks/firebaseConfig.ts
@@ -1,14 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCWYsvSn1AdSsdYIhUPZzi8GJv8vPrQkfA",
-    authDomain: "cyber-f9683.firebaseapp.com",
-    projectId: "cyber-f9683",
-    storageBucket: "cyber-f9683.firebasestorage.app",
-    messagingSenderId: "155496684192",
-    appId: "1:155496684192:web:94ee07e8b6ac57874587c5",
-};
+    apiKey: "AIzaSyDXj1bUtm1JkmEDGJTJsSM32EVM_5UPgDs",
+    authDomain: "final-year-project-250b5.firebaseapp.com",
+    projectId: "final-year-project-250b5",
+    storageBucket: "final-year-project-250b5.firebasestorage.app",
+    messagingSenderId: "945588571898",
+    appId: "1:945588571898:web:04048f189b92ec41778343"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
+
